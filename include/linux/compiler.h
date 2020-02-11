6b949db64dd89c94ba8586823d79f30095850027
Marco Elver <elver@google.com>
2020-02-11 17:04:20 +0100

    compiler.h, seqlock.h: Remove unnecessary kcsan.h includes
    No we longer have to include kcsan.h, since the required KCSAN interface
    for both compiler.h and seqlock.h are now provided by kcsan-checks.h.
    
    Acked-by: John Hubbard <jhubbard@nvidia.com>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -382,8 +382,6 @@ unsigned long read_word_at_a_time(const void *addr)
 	__u.__val;					\
 })
 
-#include <linux/kcsan.h>
-
 /**
  * data_race - mark an expression as containing intentional data races
  *
