1ad039d0694c1a3090cb1692321071f037604806
Danny Lin <danny@kdrag0n.dev>
2020-12-07 09:10:56 -0800

    cpufreq: qcom-hw: Log reported voltage for each OPP
    Required for calculating simplified energy models.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -467,8 +467,8 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 			freq = cpu_hw_rate / 1000;
 
 		c->table[i].frequency = freq;
-		dev_dbg(dev, "index=%d freq=%d, core_count %d\n",
-				i, c->table[i].frequency, core_count);
+		dev_info(dev, "index=%d freq=%d volt=%u, core_count %d\n",
+				 i, c->table[i].frequency, volt, core_count);
 
 		if (core_count != max_cores)
 			c->table[i].flags  = CPUFREQ_BOOST_FREQ;
