33c363e123fb61c1dda7235187322139109112c0
EcrosoftXiao <xy19990903@gmail.com>
2021-11-06 16:52:25 +0800

    techpack: display: Temporary disable dimlayer exposure while in high brightness mode
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -2711,7 +2711,7 @@ static int sde_crtc_config_exposure_dim_layer(struct drm_crtc_state *crtc_state,
 		return -EINVAL;
 	}
 
-	if (!alpha || panel->doze_mode_requested) {
+	if (!alpha || panel->doze_mode_requested || panel->hbm_enabled) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}
@@ -4889,7 +4889,7 @@ static int sde_crtc_exposure_atomic_check(struct sde_crtc_state *cstate,
 	struct dsi_display *dsi_display = get_main_display();
 	struct dsi_panel *panel = dsi_display->panel;
 
-	if (!panel->dimlayer_exposure || panel->doze_mode_requested) {
+	if (!panel->dimlayer_exposure || panel->doze_mode_requested || panel->hbm_enabled) {
 		cstate->exposure_dim_layer = NULL;
 		return 0;
 	}
