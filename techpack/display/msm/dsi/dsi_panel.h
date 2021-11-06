a7caf7e133218a5e0bac8adcd0ecf3b8b619179f
EcrosoftXiao <xy19990903@gmail.com>
2021-11-06 15:11:40 +0800

    techpack: display: Introduce high brightness mode
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -285,6 +285,8 @@ struct dsi_panel {
 	bool fod_ui;
 	int local_hbm_on_1000nit_51_index;
 
+	bool hbm_enabled;
+
 #ifdef CONFIG_DRM_SDE_EXPO
 	bool dimlayer_exposure;
 #endif
@@ -432,4 +434,6 @@ void dsi_panel_request_fod_hbm(struct dsi_panel *panel, bool status);
 
 int dsi_panel_set_doze_status(struct dsi_panel *panel, bool status);
 int dsi_panel_set_doze_mode(struct dsi_panel *panel, enum dsi_doze_mode_type mode);
+
+int dsi_panel_set_hbm_mode(struct dsi_panel *panel, bool status);
 #endif /* _DSI_PANEL_H_ */
