330cb785e00eda5320faebe2db776b262a320586
EndCredits <alicization.han@gmail.com>
2023-03-31 00:00:08 +0800

    ak3: Set permissions for ramdisk files
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- tools/ak3/anykernel.sh
+++ tools/ak3/anykernel.sh
@@ -30,6 +30,11 @@ patch_vbmeta_flag=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
