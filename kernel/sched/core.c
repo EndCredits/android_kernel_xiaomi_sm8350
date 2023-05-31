3daed9256b59430a21926260468b92e385d6154d
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:48 +0200

    BACKPORT: sched/debug: Rename sysctl_sched_min_granularity to sysctl_sched_base_slice
    EEVDF uses this tunable as the base request/slice -- make sure the
    name reflects this.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124604.205287511@infradead.org
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3015,7 +3015,7 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->wts.iowaited			= false;
 #endif
 	p->se.vlag			= 0;
-	p->se.slice			= sysctl_sched_min_granularity;
+	p->se.slice			= sysctl_sched_base_slice;
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
