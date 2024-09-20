1d1110228b75ceab1698e58272af4164473974fc
EndCredits <endcredits@crepuscular-aosp.icu>
2024-09-20 23:04:19 +0800

    qcacld-3.0: Mark wlan_hdd_state_ctrl_param_create as unused
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -16967,7 +16967,7 @@ const struct file_operations wlan_hdd_state_fops = {
 	.release = wlan_hdd_state_ctrl_param_release,
 };
 
-static int  wlan_hdd_state_ctrl_param_create(void)
+static __maybe_unused int  wlan_hdd_state_ctrl_param_create(void)
 {
 	unsigned int wlan_hdd_state_major = 0;
 	int ret;
